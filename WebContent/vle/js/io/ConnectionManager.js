2533fa9f80996dfc9fa2b3f79dbaf14d2b97bef9
Hiroki Terashima <honchikun@gmail.com>
2009-04-30 19:56:11 +0000

    removed alerts

--- WebContent/vle/js/io/ConnectionManager.js
+++ WebContent/vle/js/io/ConnectionManager.js
@@ -28,13 +28,10 @@ ConnectionManager.prototype.setGetURL = function(getURL) {
  * Sends the user's navigation and student data back to the vle db
  */
 ConnectionManager.prototype.post = function(workgroupId, userName, save) {
-	alert('ConnectionManager.post()');
 	
 	if(this.postURL == null) {
 		return;
 	}
-	
-	alert('after');
 	
 	var save = save;
 	var postData;
