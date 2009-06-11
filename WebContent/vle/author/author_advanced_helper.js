5bf465976ae323bc460164dc53b1c88967d90873
Hiroki Terashima <honchikun@gmail.com>
2009-06-11 00:31:45 +0000

    merged vlewrapper-1.1@483->583 to trunk -r:483:583 Part 3

---
+++ WebContent/vle/author/author_advanced_helper.js
@@ -0,0 +1,62 @@
+function save(){
+	var htmlString = document.getElementById('sourceTextArea').value;
+	
+	var callback = {
+		success: function(o){
+			saved = true;
+			if(o.responseText!='success'){
+				alert('Failed save to server');
+			};
+		},
+		failure: function(o){alert('Unable to save file to server');},
+		scope:this
+	};
+	
+	YAHOO.util.Connect.asyncRequest('POST', '../filemanager.html', callback, 'command=updateFile&param1=' + parent.projectPath + '&param2=' + parent.filename + '&param3=' + encodeURIComponent(htmlString));
+};
+
+/**
+ * Load the authoring view from the specified filename
+ * filename points to a plain old file.
+ */
+function loadAuthoringFromFile(filename, projectName, projectPath, pathSeparator) {
+	var callback =
+	{
+	  success: function(o) { 
+	  var xmlDocToParse = o.responseXML;
+	
+		/*
+		 * display the xmlString to the user in the left area so they can
+		 * edit and author it. 
+		 */
+		document.getElementById('sourceTextArea').value = o.responseText;
+	
+		//retrieve the xmlString from the mcObj
+		var xmlString = "";
+		if(window.ActiveXObject) {
+			xmlString = xmlDocToParse.xml;
+		} else {
+			xmlString = (new XMLSerializer()).serializeToString(xmlDocToParse);
+		}
+
+		window.frames["previewFrame"].loadFromXMLString(xmlString);
+
+		},
+		failure: function(o) { alert('failure');},
+		scope: this
+	}
+
+	YAHOO.util.Connect.asyncRequest('POST', '../filemanager.html', callback, 'command=retrieveFile&param1=' + projectPath + pathSeparator + filename);
+}
+
+/*
+ * Called when 'keyup' event is received on sourceTextArea on this page.
+ * gets the text in the textarea and re-renders the previewFrame
+ */
+function sourceUpdated() {
+	saved = false;
+	//retrieve the authored text
+	var xmlString = document.getElementById('sourceTextArea').value;
+
+	window.frames["previewFrame"].loadFromXMLString(xmlString);
+}
---
+++ WebContent/vle/author/author_helper.js
@@ -0,0 +1,53 @@
+var iconUrl = 'images/stepIcons/UCCP/';
+var nodeTypes = ['HtmlNode', 'BrainstormNode', 'FillinNode', 'MatchSequenceNode', 'MultipleChoiceNode', 
+		'NoteNode', 'JournalEntryNode', 'OutsideUrlNode', 'GlueNode', 'OpenResponseNode'];
+var nodeClasses = [['intro', 'curriculum', 'display', 'cartoon', 'codeit', 'simulation', 'movie', 'homework', 'summary'],
+			['brainstorm', 'qadiscuss'],
+			['fillblank'],
+			['matchsequence'],
+			['multiplechoice'],
+			['note'],
+			['journal'],
+			['www'],
+			['instantquiz', 'teacherquiz'],
+			['openresponse']];
+var nodeClassText = [['Introductory Page', 'Curriculum Page', 'Display Page', 'Cartoon Page', 'Coding Page', 'Simulation Page', 'Movie Page', 'Homework Page', 'Summary Page'],
+			['Brainstorm session', 'Q&A Discussion'],
+			['Fill the Blank'],
+			['Match & Sequence'],
+			['Multiple Choice'],
+			['Reflection Note (popup)'],
+			['Journal Question'],
+			['WWW Page'],
+			['Instant Quiz', 'Teacher Quiz'],
+			['Open Response']];
+			
+function getSaved(){
+	return saved;
+};
+
+function loaded(){
+	if(window.parent){
+		loadAuthoringFromFile(window.parent.filename, window.parent.projectName, window.parent.projectPath, window.parent.pathSeparator);
+		window.parent.childSave = save;
+		window.parent.getSaved = getSaved;
+	};
+};
+
+/**
+ * returns a <br> element
+ */
+function createBreak(){
+	return createElement(document, 'br', null);
+};
+
+/**
+ * returns a space
+ */
+function createSpace(){
+	return document.createTextNode(' ');
+};
+
+
+//used to notify scriptloader that this script has finished loading
+scriptloader.scriptAvailable(scriptloader.baseUrl + "vle/author/author_helper.js");
