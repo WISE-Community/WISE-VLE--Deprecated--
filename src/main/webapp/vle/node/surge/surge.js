e061b7b3cc8cd9c225f7d24729109d030ce157d7
Hiroki Terashima <honchikun@gmail.com>
2011-11-02 17:35:47 -0700

    added wmode to SURGE

--- src/main/webapp/vle/node/surge/surge.js
+++ src/main/webapp/vle/node/surge/surge.js
@@ -238,8 +238,9 @@ Surge.prototype.render = function() {
 		var	swfHtml = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="770" height="480" id="surge" align="middle">'
 			+ '<param name="allowScriptAccess" value="sameDomain" />'
 			+ '<param name="allowFullScreen" value="false" />'
+			+ '<param name="wmode" value="opaque" />'
 			+ '<param name="movie" value="surge.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" />'
-			+ '<embed src="surge.swf" quality="high" bgcolor="#ffffff" width="770" height="480" name="surge" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />'
+			+ '<embed src="surge.swf" wmode="opaque" quality="high" bgcolor="#ffffff" width="770" height="480" name="surge" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />'
 			+ '</object>';
 		
 		$('#swfDiv').html(swfHtml);	
