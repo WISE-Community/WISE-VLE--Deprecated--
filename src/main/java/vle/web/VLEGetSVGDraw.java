8cff99a76cb6420623bb4572f0eccc03446494f7
Geoffrey Kwan <geoffreykwan@gmail.com>
2011-12-06 14:29:54 -0800

    Removed the StepWork child tables.
    Change-Id: I247bd3c47f8f5422f5ff4ed39fd7299b868ca6f1

--- src/main/java/vle/web/VLEGetSVGDraw.java
+++ src/main/java/vle/web/VLEGetSVGDraw.java
@@ -14,8 +14,6 @@ import org.json.JSONObject;
 
 import vle.VLEServlet;
 import vle.domain.work.StepWork;
-import vle.domain.work.StepWorkHtml;
-import vle.domain.work.StepWorkSVGDraw;
 
 /**
  * @author hirokiterashima
@@ -34,10 +32,11 @@ public class VLEGetSVGDraw extends VLEServlet {
 		
 		// for handing svg request
 		if (type != null && type.equals("SVGDrawNode") && stepWorkId != null) {
-			StepWorkSVGDraw stepWorkSVG = (StepWorkSVGDraw) StepWork.getById(new Long(stepWorkId), StepWorkSVGDraw.class);
-			svgString = stepWorkSVG.getSVGString();
+			StepWork stepWorkSVG = (StepWork) StepWork.getById(new Long(stepWorkId), StepWork.class);
+			String data = stepWorkSVG.getData();
+			svgString = getSVGString(data);
 		} else if (type != null && type.equals("MySystemNode") && stepWorkId != null) {
-			StepWorkHtml stepWork = (StepWorkHtml) StepWork.getById(new Long(stepWorkId), StepWorkHtml.class);
+			StepWork stepWork = (StepWork) StepWork.getById(new Long(stepWorkId), StepWork.class);
 				try {
 					JSONObject data = new JSONObject(stepWork.getData());
 					svgString = (String) data.getJSONArray("nodeStates").getJSONObject(0).get("data");
@@ -47,5 +46,18 @@ public class VLEGetSVGDraw extends VLEServlet {
 		}
 		response.setContentType("text/xml");
 		response.getWriter().print(svgString);
+	}
+	
+	// returns the first nodeState
+	public String getSVGString(String dataString) {
+		try {
+			JSONObject data = new JSONObject(dataString);
+			Object svgString = data.getJSONArray("nodeStates").getJSONObject(0).getJSONObject("data").get("svgString");
+			//return (String) svgString;
+			return svgString.toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return "";
+		}		
 	}
 }
