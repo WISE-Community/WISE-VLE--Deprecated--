fb76cfca1bafdaf23d1b73ed5c8cab6fe0353016
breity <breity10@gmail.com>
2012-07-25 23:36:49 -0400

    additions/corrections to insertTooltips util function

--- src/main/webapp/vle/view/view_utils.js
+++ src/main/webapp/vle/view/view_utils.js
@@ -1208,85 +1208,101 @@ View.prototype.getAnnotationsByType = function(annotationType) {
  * 
  * @param options An object to specify default miniTip settings for all tooltips (Optional; 
  * see http://goldfirestudios.com/blog/81/miniTip-jQuery-Plugin for allowable options)
+ * @param target A jQuery DOM object on which to process elements (Optional; default is entire page)
  * 
- * Individual tooltip options can be customized by adding additional attributes to the target DOM 
- * element (Optional; will override default settings):
+ * Individual tooltip options can be customized by adding additional attributes to the DOM element 
+ * (Optional; will override default settings):
  * - tooltip-event:'click' sets the tooltip to render on mouse click (vs. hover, which is the default)
  * - tooltip-anchor:'bottom', 'left', and 'right' set the positions of the tooltip to bottom, left, 
  * and right respectively (default is top)
- * - tooltip-maxW:'XXXpx' sets the max-width of the tooltip element to XXX pixels (default is '250px');
+ * - tooltip-maxw:'XXXpx' sets the max-width of the tooltip element to XXX pixels (default is '400px');
  * - tooltip-content: String (or HTML String) to set as the tooltip's content (default is the element's 
  * title attribute)
  * - tooltip-title: String to set as the tooltip's title (default is no title)
  * - tooltip-class: String to add to the tooltip element's css class (default is none)
+ * - tooltip-offset: 'XX' sets as the offset of the tooltip element to XX pixels (default is '0')
  */
-View.prototype.insertTooltips = function(options){
-	// for all DOM elements with the 'tooltip' class, initialize miniTip
-	$('.tooltip').each(function(){
+View.prototype.insertTooltips = function(options,target){
+	
+	function processElement(item,options){
 		// set miniTip default options
-		var settings = {};
+		var settings = {
+			anchor:'n',
+			event:'hover',
+			aHide:false,
+			maxW:'400px',
+			offset:1,
+			fadeIn:10,
+			fadOut:10,
+			delay:100,
+			show: function(){
+				$('#miniTip').width('auto');
+			}
+		};
 		if(options != null && typeof options == 'object'){
-			// options have been sent in as a parameter
-			settings = options;
-		} else {
-			// options have not been sent in as a paremeter, so set them 
-			settings = {
-				anchor:'n',
-				event:'hover',
-				aHide:false,
-				maxW:'250px',
-				fadeIn:10,
-				fadOut:10,
-				delay:100,
-				show: function(){},
-				hide: function(){}
-			};
+			// miniTip options have been sent in as a parameter, so merge with defaults
+			$.extend(settings,options);
 		}
 		
 		// set options based on target element attributes
-		if($(this).attr('tooltip-event') == 'click'){
+		if(item.attr('tooltip-event') == 'click'){
 			settings.event = 'click';
 		}
-		if($(this).attr('tooltip-anchor') == 'right'){
+		if(item.attr('tooltip-anchor') == 'right'){
 			settings.anchor = 'e';
-		} else if ($(this).attr('tooltip-anchor') == 'bottom'){
+		} else if (item.attr('tooltip-anchor') == 'bottom'){
 			settings.anchor = 's';
-		} else if ($(this).attr('tooltip-anchor') == 'left'){
+		} else if (item.attr('tooltip-anchor') == 'left'){
 			settings.anchor = 'w';
 		}
-		if($(this).attr('tooltip-maxW') && $(this).attr('tooltip-maxW').match(/^[0-9]+px$/)){
-			settings.maxW = $(this).attr('tooltip-maxW');
+		if(item.attr('tooltip-maxw') && item.attr('tooltip-maxw').match(/^[0-9]+px$/)){
+			settings.maxW = item.attr('tooltip-maxw');
 		}
-		if(typeof $(this).attr('tooltip-content') == 'string'){
-			settings['content'] = $(this).attr('tooltip-content');
+		if(typeof item.attr('tooltip-content') == 'string'){
+			settings['content'] = item.attr('tooltip-content');
 		}
-		if(typeof $(this).attr('tooltip-title') == 'string'){
-			settings['title'] = $(this).attr('tooltip-title');
+		if(typeof item.attr('tooltip-title') == 'string'){
+			settings['title'] = item.attr('tooltip-title');
 		}
-		if(typeof $(this).attr('tooltip-class') == 'string'){
-			var doShow = settings.show, doHide = settings.hide;
+		if(typeof item.attr('tooltip-offset') == 'string'){
+			settings['offset'] = parseInt(item.attr('tooltip-offset'));
+		}
+		if(typeof item.attr('tooltip-class') == 'string'){
+			var doShow = settings.show, newClass = item.attr('tooltip-class');
 			settings.show = function(){
-				$('#miniTip').addClass($(this).attr('tooltip-class'));
+				$('#miniTip').attr('class',''); // clear out existing class
+				$('#miniTip').addClass(newClass);
 				doShow();
-			};
-			settings.hide = function(){
-				setTimeout(function(){$('#miniTip').removeClass($(this).attr('tooltip-class'));},200);
-				doHide();
 			};
 		} else {
 			var doShow = settings.show;
 			settings.show = function(){
-				$('#miniTip').attr('class','');
+				$('#miniTip').attr('class',''); // clear out existing class
 				doShow();
 			};
 		}
 		
 		// initialize miniTip on element
-		$(this).miniTip(settings);
+		item.miniTip(settings);
 		
 		// remove all tooltip attributes and class from DOM element (to clean up html and so item are not re-processed if insertTooltips is called again on same page)
-		$(this).removeAttr('tooltip-event').removeAttr('tooltip-anchor').removeAttr('tooltip-maxW').removeAttr('tooltip-content').removeAttr('tooltip-title').removeClass('tooltip');
-	});
+		item.removeAttr('tooltip-event').removeAttr('tooltip-anchor').removeAttr('tooltip-maxw').removeAttr('tooltip-content').removeAttr('tooltip-class').removeAttr('tooltip-offset').removeAttr('tooltip-title').removeClass('tooltip');
+	}
+	
+	// for all DOM elements with the 'tooltip' class, initialize miniTip
+	if(target){
+		if(target.hasClass('tooltip')){
+			processElement(target,options);
+		} else {
+			$('.tooltip',target).each(function(){
+				processElement($(this),options);
+			});
+		}
+	} else {
+		$('.tooltip').each(function(){
+			processElement($(this),options);
+		});
+	}
 };
 
 /**
