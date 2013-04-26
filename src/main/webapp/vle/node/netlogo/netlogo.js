af785ddb7e5c94a967607d3993611cba56e55ae4
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-04-26 16:23:17 -0400

    netlogo-inquiry-logging: updated commit/missing files
    Some of my changes were in the tomcat dir instead of
    the working dir for the VLE repository.

--- src/main/webapp/vle/node/netlogo/netlogo.js
+++ src/main/webapp/vle/node/netlogo/netlogo.js
@@ -1,12 +1,12 @@
-
 /**
  * @constructor
  * @param node
  * @returns
  */
-function Netlogo(node) {
-	this.node = node;
-	this.content = node.getContent().getContentJSON();
+function Netlogo(node, view) {
+  this.node = node;
+	this.view = view;
+  this.content = node.getContent().getContentJSON();
   this.applet = '';
   this.nlObjPanel = null;
   this.nlObjWorkspace = null;
@@ -15,117 +15,131 @@ function Netlogo(node) {
   this.nlObjObserver = null;
   this.nlObjGlobals = null;
   this.nlGlobals = null;
-  this.clearDataReady = null;
 }
 
-Netlogo.prototype.render = function(){
-	//display any into content to the student
-	$('#promptDiv').html(this.content.prompt);
+Netlogo.prototype = {
 
-	// load the Netlogo jar and activity
-	var archive = 'NetLogoLite5.jar';
-	if(this.content.version == '4'){
-		archive = 'NetLogoLite.jar';
-	}
+  render: function() {
+    //display any into content to the student
+    $('#promptDiv').html(this.content.prompt);
+
+    // load the Netlogo jar and activity
+    var archive = 'NetLogoLite5.jar';
+    if(this.content.version == '4'){
+      archive = 'NetLogoLite.jar';
+    }
 
     //handle the case that there is no uri selected yet.
     var model = '';
     var appletStr = '';
 
     if(this.content.activity_uri !== '') {
-		model = '<param name="DefaultModel" value="' + this.content.activity_uri + '">';
+      model = '<param name="DefaultModel" value="' + this.content.activity_uri + '">';
 
-		appletStr = '<applet id="netlogo-applet" code="org.nlogo.lite.Applet" codebase="/vlewrapper/vle/node/netlogo/"' +
-        'archive="' + archive + '" width="' + this.content.width + '" height="' + this.content.height + '">' +
-         model + '</applet>';
-	}
+      appletStr = '<applet id="netlogo-applet" code="org.nlogo.lite.Applet" codebase="/vlewrapper/vle/node/netlogo/"' +
+      'archive="' + archive + '" width="' + this.content.width + '" height="' + this.content.height + '">' +
+      model + '</applet>';
+    }
     $('#netlogo_wrapper').html(appletStr);
     this.applet = document.getElementById("netlogo-applet");
-    this.applet.ready = false;
-    this.applet.checkedMoreThanOnce = false;
-    window.setTimeout(this.appletReady, 250);
-
-};
-
-function stripWhiteSpace(str) {
-  return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-}
-
-Netlogo.prototype.appletReady = function() {
-  var self = this,
-      globalsStr;
-
-  self.applet.ready = false;
-
-  try {
-    self.nlObjPanel     = self.applet.panel();                                           // org.nlogo.lite.Applet object
-    self.nlObjWorkspace = self.nl_obj_panel.workspace();                                 // org.nlogo.lite.LiteWorkspace
-    self.nlObjWorld     = self.nl_obj_workspace.org$nlogo$lite$LiteWorkspace$$world;     // org.nlogo.agent.World
-    self.nlObjProgram   = self.nl_obj_world.program();                                   // org.nlogo.api.Program
-    self.nlObjObserver  = self.nl_obj_world.observer();
-    self.nlObjGlobals   = self.nl_obj_program.globals();
-    globalsStr = self.nlObjGlobals.toString();
-    self.nlGlobals = globalsStr.substr(1, globalsStr.length-2).split(",").map(function(e) { return stripWhiteSpace(e); });
-    if (self.nlGlobals.length > 1) {
-      self.applet.ready = true;
+    if (this.applet) {
+      this.applet.ready = false;
+      this.applet.checkedMoreThanOnce = false;
+      window.setTimeout(this.nlAppletReady.bind(this), 250);
     }
-  } catch (e) {
-    // applet is not ready
+  },
+
+  save: function() {
+    // Try and get the inquiry data from the NetLogo model for up to 0.5s
+    var modelData = {},
+        exportReady = false,
+        stillChecking = true,
+        startTime,
+        elapsedTime = 0;
+
+    if (this.nlDataExportModuleAvailable()) {
+      this.nlCmdExecute("data-export:make-model-data");
+      startTime = Date.now();
+      // for some reason we have to wait here 200ms
+      while(elapsedTime < 200) {
+        elapsedTime = Date.now() - startTime;
+      }
+      // check for another 300ms for data to be available
+      while(elapsedTime < 500) {
+        exportReady = this.nlDataReady();
+        if (exportReady) {
+          modelData = this.nlGetExportedData();
+          break;
+        }
+        elapsedTime = Date.now() - startTime;
+      }
+    }
+    jsonStr = JSON.stringify(modelData);
+    nlState = new NetlogoState(jsonStr);
+    console.log("save, jsonStr:" + jsonStr);
+    /*
+    * fire the event to push this state to the global view.states object.
+    * the student work is saved to the server once they move on to the
+    * next step.
+    */
+    this.view.pushStudentWork(this.node.id, nlState);
+  },
+
+  nlAppletReady: function() {
+    var globalsStr;
+
+    function stripWhiteSpace(str) {
+      return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    }
+
+    this.applet.ready = false;
+
+    try {
+      this.nlObjPanel     = this.applet.panel();                                         // org.nlogo.lite.Applet object
+      this.nlObjWorkspace = this.nlObjPanel.workspace();                                 // org.nlogo.lite.LiteWorkspace
+      this.nlObjWorld     = this.nlObjWorkspace.org$nlogo$lite$LiteWorkspace$$world;     // org.nlogo.agent.World
+      this.nlObjProgram   = this.nlObjWorld.program();                                   // org.nlogo.api.Program
+      this.nlObjObserver  = this.nlObjWorld.observer();
+      this.nlObjGlobals   = this.nlObjProgram.globals();
+      globalsStr = this.nlObjGlobals.toString();
+      this.nlGlobals = globalsStr.substr(1, globalsStr.length-2).split(",").map(function(e) { return stripWhiteSpace(e); });
+      if (this.nlGlobals.length > 1) {
+        this.applet.ready = true;
+      }
+    } catch (e) {
+      // applet is not ready
+    }
+
+    if (!this.applet.ready) {
+      this.applet.checkedMoreThanOnce = window.setTimeout(this.nlAppletReady.bind(this), 250);
+    }
+
+    return this.applet.ready;
+  },
+
+  nlDataExportModuleAvailable: function() {
+    return this.nlReadGlobal("DATA-EXPORT:MODULE-AVAILABLE");
+  },
+
+  nlDataReady: function() {
+    return this.nlReadGlobal("DATA-EXPORT:DATA-READY?");
+  },
+
+  nlCmdExecute: function(cmd) {
+    this.nlObjPanel.commandLater(cmd);
+  },
+
+  nlReadGlobal: function(global) {
+    if (this.nlGlobals.indexOf(global) < 0) return null;
+    return this.nlObjObserver.getVariable(this.nlGlobals.indexOf(global));
+  },
+
+  nlGetExportedData: function() {
+    return this.nlReadGlobal("DATA-EXPORT:MODEL-DATA");
   }
-
-  if (!self.applet.ready) {
-    self.applet.checkedMoreThanOnce = window.setTimeout(self.appletReady, 250);
-  }
-
-  return applet.ready;
 };
-
-Netlogo.prototype.nlDataReady = function() {
-  var self = this;
-  return self.nlReadGlobal("DATA-EXPORT:DATA-READY?");
-};
-
-Netlogo.prototype.nlCmdExecute = function(cmd) {
-  var self = this;
-  self.nlObjPanel.commandLater(cmd);
-};
-
-Netlogo.prototype.nlReadGlobal = function(global) {
-  var self = this;
-  if (self.nlGlobals.indexOf(global) < 0) return null;
-  return self.nlObjObserver.getVariable(self.nlGlobals.indexOf(global));
-};
-
-Netlogo.prototype.getExportedData = function() {
-  var self = this;
-  return self.nlReadGlobal("DATA-EXPORT:MODEL-DATA");
-};
-
-Netlogo.prototype.exportDataHandler = function() {
-  var self = this;
-  self.nlCmdExecute("data-export:make-model-data");
-  self.clearDataReady = window.setInterval(this.exportDataReadyCallback, 250);
-};
-
-Netlogo.prototype.exportDataReadyCallback = function() {
-  var self = this,
-      modelData,
-      dataSeries,
-      runSeries,
-      exportDone = this.nlDataReady();
-
-  if (exportDone) {
-    clearInterval(self.clearDataReady);
-    modelData = self.nlReadGlobal("DATA-EXPORT:DATA-READY?");
-    console.log(modelData);
-  }
-};
-
-
- //+ this.content.activity_uri + '">' +
- //switched to default to the 5.0 version.
 
 //used to notify scriptloader that this script has finished loading
 if(typeof eventManager != 'undefined'){
-	eventManager.fire('scriptLoaded', 'vle/node/netlogo/netlogo.js');
+  eventManager.fire('scriptLoaded', 'vle/node/netlogo/netlogo.js');
 }
