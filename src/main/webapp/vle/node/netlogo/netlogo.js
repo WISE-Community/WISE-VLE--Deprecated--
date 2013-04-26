9f304a09758533dd92fe6986e64cc700fc341029
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-04-26 15:53:38 -0400

    support optional inquiry-data logging in NetLogo step
    If a NetLogo model is used that has integrated the 
    inquiry-data logging module developed for the VISUAL
    project then this updated NetLogo step type will extract
    the inquiry-log data and stave it in the step session.
    
    See this example of an AirBag model that has integrated
    the inquiry-data logging module.
    
    https://gist.github.com/stepheneb/5469959
    
    The integrated inquiry-data logging module is derived from
    the work shown here developed for the VISUAL project:
    
    http://visual.concord.org/experiments/netlogo-visual/models.html
    
    In these examples the module is loaded separately from
    the actual NetLogo model with the following statement in the
    very beginning of the NetLogo code for the model:
    
      __includes [ "data-export-modular.nls" ]
    
    Here's where these resources/examples are available in a code repository:
    https://github.com/concord-consortium/lab/tree/WIP-refactor-properties/src/experiments/netlogo-visual/models
    
    However there is a bug in the Windows version of the
    NetLogo applet and the only way an external module can
    be loaded is if it comes from the same directory location
    as the NetLogo *.logo file.
    
    If the data-export-modular.nls module was included as a
    resource in this step the web application would need to
    synthesize a path for any NetLogo applet that requested
    it so it appeared to the NetLogo applet that data-export-modular.nls
    was being served from the same directory.
    
    This of course would be a big improvement because with
    this initial implementation the code in data-export-modular.nls
    needs to be copied into every NetLogo applet that uses it.
    
    If there is a bug fixed in data-export-modular.nls each
    NetLogo applet that uses it will need to be updated.

--- src/main/webapp/vle/node/netlogo/netlogo.js
+++ src/main/webapp/vle/node/netlogo/netlogo.js
@@ -7,9 +7,16 @@
 function Netlogo(node) {
 	this.node = node;
 	this.content = node.getContent().getContentJSON();
-};
-
-
+  this.applet = '';
+  this.nlObjPanel = null;
+  this.nlObjWorkspace = null;
+  this.nlObjWorld = null;
+  this.nlObjProgram = null;
+  this.nlObjObserver = null;
+  this.nlObjGlobals = null;
+  this.nlGlobals = null;
+  this.clearDataReady = null;
+}
 
 Netlogo.prototype.render = function(){
 	//display any into content to the student
@@ -23,18 +30,95 @@ Netlogo.prototype.render = function(){
 
     //handle the case that there is no uri selected yet.
     var model = '';
-    var applet = '';
-    
-    if(this.content.activity_uri != '') {
+    var appletStr = '';
+
+    if(this.content.activity_uri !== '') {
 		model = '<param name="DefaultModel" value="' + this.content.activity_uri + '">';
-		
-		applet = '<applet code="org.nlogo.lite.Applet" codebase="/vlewrapper/vle/node/netlogo/"' +  
+
+		appletStr = '<applet id="netlogo-applet" code="org.nlogo.lite.Applet" codebase="/vlewrapper/vle/node/netlogo/"' +
         'archive="' + archive + '" width="' + this.content.width + '" height="' + this.content.height + '">' +
-         model + 
-        '</applet>';
+         model + '</applet>';
 	}
-    
-    $('#netlogo_wrapper').html(applet);
+    $('#netlogo_wrapper').html(appletStr);
+    this.applet = document.getElementById("netlogo-applet");
+    this.applet.ready = false;
+    this.applet.checkedMoreThanOnce = false;
+    window.setTimeout(this.appletReady, 250);
+
+};
+
+function stripWhiteSpace(str) {
+  return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+}
+
+Netlogo.prototype.appletReady = function() {
+  var self = this,
+      globalsStr;
+
+  self.applet.ready = false;
+
+  try {
+    self.nlObjPanel     = self.applet.panel();                                           // org.nlogo.lite.Applet object
+    self.nlObjWorkspace = self.nl_obj_panel.workspace();                                 // org.nlogo.lite.LiteWorkspace
+    self.nlObjWorld     = self.nl_obj_workspace.org$nlogo$lite$LiteWorkspace$$world;     // org.nlogo.agent.World
+    self.nlObjProgram   = self.nl_obj_world.program();                                   // org.nlogo.api.Program
+    self.nlObjObserver  = self.nl_obj_world.observer();
+    self.nlObjGlobals   = self.nl_obj_program.globals();
+    globalsStr = self.nlObjGlobals.toString();
+    self.nlGlobals = globalsStr.substr(1, globalsStr.length-2).split(",").map(function(e) { return stripWhiteSpace(e); });
+    if (self.nlGlobals.length > 1) {
+      self.applet.ready = true;
+    }
+  } catch (e) {
+    // applet is not ready
+  }
+
+  if (!self.applet.ready) {
+    self.applet.checkedMoreThanOnce = window.setTimeout(self.appletReady, 250);
+  }
+
+  return applet.ready;
+};
+
+Netlogo.prototype.nlDataReady = function() {
+  var self = this;
+  return self.nlReadGlobal("DATA-EXPORT:DATA-READY?");
+};
+
+Netlogo.prototype.nlCmdExecute = function(cmd) {
+  var self = this;
+  self.nlObjPanel.commandLater(cmd);
+};
+
+Netlogo.prototype.nlReadGlobal = function(global) {
+  var self = this;
+  if (self.nlGlobals.indexOf(global) < 0) return null;
+  return self.nlObjObserver.getVariable(self.nlGlobals.indexOf(global));
+};
+
+Netlogo.prototype.getExportedData = function() {
+  var self = this;
+  return self.nlReadGlobal("DATA-EXPORT:MODEL-DATA");
+};
+
+Netlogo.prototype.exportDataHandler = function() {
+  var self = this;
+  self.nlCmdExecute("data-export:make-model-data");
+  self.clearDataReady = window.setInterval(this.exportDataReadyCallback, 250);
+};
+
+Netlogo.prototype.exportDataReadyCallback = function() {
+  var self = this,
+      modelData,
+      dataSeries,
+      runSeries,
+      exportDone = this.nlDataReady();
+
+  if (exportDone) {
+    clearInterval(self.clearDataReady);
+    modelData = self.nlReadGlobal("DATA-EXPORT:DATA-READY?");
+    console.log(modelData);
+  }
 };
 
 
@@ -44,4 +128,4 @@ Netlogo.prototype.render = function(){
 //used to notify scriptloader that this script has finished loading
 if(typeof eventManager != 'undefined'){
 	eventManager.fire('scriptLoaded', 'vle/node/netlogo/netlogo.js');
-};
+}
