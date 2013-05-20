1c858b4b515fbfd4ef17babf2b7419f48f7c08e0
breity <breity10@gmail.com>
2013-05-20 11:10:40 -0700

    Updated the svg-edit description extension for svg-edit 2.6 and some style changes

--- src/main/webapp/vle/node/draw/svg-edit/extensions/ext-description.js
+++ src/main/webapp/vle/node/draw/svg-edit/extensions/ext-description.js
@@ -1,12 +1,15 @@
 /*
  * ext-description.js
  *
- * Licensed under the Apache License, Version 2
+ * Licensed under the MIT License
  *
- * Copyright(c) 2010 Jonathan Breitbart
+ * Copyright(c) 2013 Jonathan Lim-Breitbart
  *
  * Allows users to add a description/annotation to their drawing
+ * Requires 'ext-description.css' to be included in 'extension' directory
  * JQuery UI with dialogs and sliders plus accompanying css also required
+ * 
+ * TODO: i18n
  */
 
 var descriptionLoaded = false; // wise4 var to indicate when extension has finished loading
@@ -17,34 +20,52 @@ svgEditor.addExtension("Description", function(S) {
 	
 	function setupDisplay(){
 		
+		svgEditor.toggleDescription = function(close){
+			if(close){
+				$('#description').css('height','30px');
+				$('#description_content').hide();
+				$('#description_edit').show();
+				$('#description_collapse').hide();
+				$('#description span.maximized').hide();
+				$('#description span.minimized').show();
+			}
+			else {
+				$('#description').css('height','125px');
+				$('#description_content').css('height','75px').show().focus();
+				$('#description_edit').hide();
+				$('#description_collapse').show();
+				$('#description span.minimized').hide();
+				$('#description span.maximized').show();
+			}
+		};
+		
 		var displaytext = '<div id="description"><div id="description_wrapper">'+
-			'<div class="description_header"><div class="description_header_text"><span class="panel_title">Description:</span>'+
-			' <span class="maximized">(Enter your text in the box below)</span><span class="minimized"></span>'+
+			'<div class="description_header" title="Click to edit"><div class="description_header_text"><span class="panel_title">Description:</span>'+
+			' <span class="maximized">(Enter your text in the box below)</span><span class="minimized">(Click to add)</span>'+
 			'</div><div class="description_buttons">'+
 			'<a id="description_edit" title="Edit Description">Edit</a>'+
-			'<button id="description_collapse" style="display:none" title="Save">Save</button></div></div>'+
+			'<button id="description_collapse" class="ui-button ui-state-default" title="Save">Save</button></div></div>'+
 			'<textarea id="description_content" class="description_input"></textarea></div></div>';
 		
 		$('#workarea').append(displaytext);
 		
-		$('#description_content').keyup(function(){
+		$('#description_content').on('keyup', function(event){
 			var value = $('#description_content').val();
-			$('#description span.minimized').text(value);
-			$('#description span.minimized').attr('title');
+			if(value && value !== ''){
+				$('#description span.minimized').text(value);
+			} else {
+				$('#description span.minimized').text('(Click to add)');
+			}
 			svgEditor.description = value;
 			svgEditor.changed = true;
 		});
 		
-		$('#description_edit, .description_header span').click(function(){
-			$('#description_content').focus();
-		});
-		
-		$('#description_collapse').click(function(){
-			svgEditor.toggleDescription(true);
-		});
-		
-		$('#description_content').focus(function(){
+		$('#description_edit, .description_header span').on('click', function(){
 			svgEditor.toggleDescription(false);
+		});
+		
+		$('#description_collapse').on('click', function(){
+			svgEditor.toggleDescription(true);
 		});
 		
 		//$('#workarea').css('bottom','100px');
@@ -52,26 +73,6 @@ svgEditor.addExtension("Description", function(S) {
 		svgEditor.toggleDescription(true);
 		
 		descriptionLoaded = true;
-	};
-	
-	svgEditor.toggleDescription = function(close){
-		if(close){
-			$('#description').css('height','28px');
-			$('#description_content').hide();
-			$('#description_edit').show();
-			$('#description_collapse').hide();
-			$('#description span.maximized').hide();
-			$('#description span.minimized').show();
-		}
-		else {
-			$('#description').css('height','127px');
-			$('#description_content').css('height','75px');
-			$('#description_content').show();
-			$('#description_edit').hide();
-			$('#description_collapse').show();
-			$('#description span.minimized').hide();
-			$('#description span.maximized').show();
-		}
 	};
 	
 	return {
