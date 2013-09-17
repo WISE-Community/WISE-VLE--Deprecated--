83de7fef3d0cfa507fdcf5b6932d292e0cdeba20
breity <breity10@gmail.com>
2013-09-17 11:00:24 -0700

    Updated icon options for EPIGAME step type

--- src/main/webapp/vle/node/epigame/setup.js
+++ src/main/webapp/vle/node/epigame/setup.js
@@ -35,10 +35,26 @@ var dependencies = [
 ];
 
 var nodeClasses = [
-	{nodeClass:'mission', nodeClassText:'Generic Mission'},
-	{nodeClass:'bronze', nodeClassText:'Easy Mission'},
-	{nodeClass:'silver', nodeClassText:'Medium Mission'},
-	{nodeClass:'gold', nodeClassText:'Hard Mission'}
+	{nodeClass:'mission-easy', nodeClassText:'Easy Mission', icon:'node/epigame/icons/planet-easy.png'},
+	{nodeClass:'mission-easy-bronze', nodeClassText:'Easy Mission (Bronze)', icon:'node/epigame/icons/planet-easy-bronze.png'},
+	{nodeClass:'mission-easy-silver', nodeClassText:'Easy Mission (Silver)', icon:'node/epigame/icons/planet-easy-silver.png'},
+	{nodeClass:'mission-easy-gold', nodeClassText:'Easy Mission (Gold)', icon:'node/epigame/icons/planet-easy-gold.png'},
+	{nodeClass:'mission-medium', nodeClassText:'Medium Mission', icon:'node/epigame/icons/planet-medium.png'},
+	{nodeClass:'mission-medium-bronze', nodeClassText:'Medium Mission (Bronze)', icon:'node/epigame/icons/planet-medium-bronze.png'},
+	{nodeClass:'mission-medium-silver', nodeClassText:'Medium Mission (Silver)', icon:'node/epigame/icons/planet-medium-silver.png'},
+	{nodeClass:'mission-medium-gold', nodeClassText:'Medium Mission (Gold)', icon:'node/epigame/icons/planet-medium-gold.png'},
+	{nodeClass:'mission-hard', nodeClassText:'Hard Mission', icon:'node/epigame/icons/planet-hard.png'},
+	{nodeClass:'mission-hard-bronze', nodeClassText:'Hard Mission (Bronze)', icon:'node/epigame/icons/planet-hard-bronze.png'},
+	{nodeClass:'mission-hard-silver', nodeClassText:'Hard Mission (Silver)', icon:'node/epigame/icons/planet-hard-silver.png'},
+	{nodeClass:'mission-hard-gold', nodeClassText:'Hard Mission (Gold)', icon:'node/epigame/icons/planet-hard-gold.png'},
+	{nodeClass:'mission-locked', nodeClassText:'Locked Mission', icon:'node/epigame/icons/planet-locked.png'},
+	{nodeClass:'warp-blue', nodeClassText:'Blue Warp Mission', icon:'node/epigame/icons/warp-blue.png'},
+	{nodeClass:'warp-green', nodeClassText:'Green Warp Mission', icon:'node/epigame/icons/warp-green.png'},
+	{nodeClass:'warp-red', nodeClassText:'Red Warp Mission', icon:'node/epigame/icons/warp-red.png'},
+	{nodeClass:'star-bronze', nodeClassText:'Bronze Star', icon:'node/epigame/icons/star-bronze.png'},
+	{nodeClass:'star-silver', nodeClassText:'Silver Star', icon:'node/epigame/icons/star-silver.png'},
+	{nodeClass:'star-gold', nodeClassText:'Gold Star', icon:'node/epigame/icons/star-gold.png'},
+	{nodeClass:'star-locked', nodeClassText:'Locked Star', icon:'node/epigame/icons/star-locked.png'}
 ];
 
 var nodeIconPath = 'node/epigame/icons/';
