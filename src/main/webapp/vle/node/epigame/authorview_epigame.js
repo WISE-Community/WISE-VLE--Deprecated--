6e1e577154961e9b642100899b3327ca372ea6c3
Brandon Smith <bsmith@filamentgames.com>
2013-09-24 10:44:13 -0500

    Epigame V2.2 FS7

--- src/main/webapp/vle/node/epigame/authorview_epigame.js
+++ src/main/webapp/vle/node/epigame/authorview_epigame.js
@@ -30,7 +30,7 @@ View.prototype.EpigameNode = {};
  */
 View.prototype.EpigameNode.commonComponents = [];
 
-View.prototype.EpigameNode.modes = ["mission", "tutorial", "adaptiveMission", "adaptiveQuiz", "editor", "map", "adaptivePostQuiz"];
+View.prototype.EpigameNode.modes = ["mission", "tutorial", "adaptiveMission", "adaptiveQuiz", "editor", "map"];
 
 /**
  * Generates the authoring page. This function will create the authoring
@@ -59,22 +59,8 @@ View.prototype.EpigameNode.generatePage = function(view){
 
 	//create a new div that will contain the authoring components
 	var pageDiv = createElement(document, 'div', {id:'dynamicPage', style:'width:100%;height:100%'});
-	
 	var authoringSwfDiv = createElement(document, 'div', {id: 'authoringSwfDiv'});
-	
-	/*
-	//create the label for the radio buttons that the author will use to select the swf source option
-	var sourceDiv = $(document.createElement('div')).attr('id','sourceDiv');
-	var sourceLabel = $(document.createElement('div')).text('SURGE activity source file (swf):');
-	
-	//create the radio buttons and labels for each source option
-	var sourceLabelDefault = $(document.createElement('span')).text('Default (use level editor)');
-	var sourceRadioDefault = $(createElement(document, 'input', {id: 'defaultSource', type: 'radio', name: 'sourceSelect', value: 'false'})).prop('checked',true);
-	var sourceLabelCustom = $(document.createElement('span')).text('Custom');
-	var sourceRadioCustom = $(createElement(document, 'input', {id: 'customSource', type: 'radio', name: 'sourceSelect', value: 'true'}));
-	sourceDiv.append(sourceLabel).append(sourceRadioDefault).append(sourceLabelDefault).append(sourceRadioCustom).append(sourceLabelCustom);
-	*/
-	
+		
 	//Custom SWF selection
 	var swfUrlDiv = $(createElement(document, 'div', {id:'swfUrlDiv'}));
 	var swfUrlLabel = $(document.createElement('span')).text('Custom swf file (Leave blank for default):');
@@ -96,7 +82,7 @@ View.prototype.EpigameNode.generatePage = function(view){
 	//create here text to use for the question time limit
 	var questionDiv = $(createElement(document, 'fieldset', {id:'questionDiv'}));	
 	var questionLegend = $(document.createElement('legend')).text('Time Limit');
-	var questionSpan = $(document.createElement('span')).text('Quiz Time Limit');	
+	var questionSpan = $(document.createElement('span')).text('Quiz Time Limit');
 	
 	var noTimeToggle = createElement(document, "input", {id:'noTimeToggle', type:"radio", name: "timeLimit", onclick:"eventManager.fire('epigameChangeSettings')"});
 	var noTimeLabel = $(createElement(document, 'label', {id:'noTimeLabel', "for":"noTimeToggle"})).text('No Time Limit');	
@@ -107,19 +93,25 @@ View.prototype.EpigameNode.generatePage = function(view){
 	var questionTimeToggle = createElement(document, "input", {id:'questionTimeToggle', type:"radio", name: "timeLimit", onclick:"eventManager.fire('epigameChangeSettings')"});
 	var questionTimeLabel = $(createElement(document, 'label', {id:'questionTimeLabel', "for":"questionTimeToggle"})).text('Time Limit per Question');	
 	var questionTimeBox = createElement(document, "input", {id:'questionTimeText', type:"text", onchange:"eventManager.fire('epigameChangeSettings')",size:"5"});
-	
-/*	
-	questionDiv.append(questionLegend);
-	questionDiv.append(noTimeToggle).append(noTimeLabel).append(createBreak());
-	questionDiv.append(testTimeToggle).append(testTimeLabel).append(createBreak());
-	questionDiv.append(questionTimeToggle).append(questionTimeLabel);
-*/	
-	
+		
 	//Mission data input
 	var levelStringDiv = $(createElement(document, 'div', {id:'levelStringDiv'}));
 	var levelStringLabel = $(createElement(document, 'span', {id:'levelStringLabel'})).text('Mission String:');
 	var levelStringTextArea = createElement(document, 'input', {id: 'levelStringTextArea', type: 'text', size: '45', onchange:"eventManager.fire('epigameUpdateLevelString')"});
 	levelStringDiv.append(levelStringLabel).append(createBreak()).append(levelStringTextArea);
+	
+	//Quiz data input
+	var quizStringDiv = $(createElement(document, 'div', {id:'quizStringDiv'}));
+	var quizStringLabel = $(createElement(document, 'span', {id:'quizStringLabel'})).text('Quiz Selector:');
+	var preQuizToggle = createElement(document, "input", {id:'preQuizToggle', type:"radio", name: "timeLimit", onclick:"eventManager.fire('epigameChangeSettings')"});
+	var preQuizLabel = $(createElement(document, 'label', {id:'preQuizLabel', "for":"noTimeToggle"})).text('No Time Limit');	
+	var postQuizToggle = createElement(document, "input", {id:'postQuizToggle', type:"radio", name: "timeLimit", onclick:"eventManager.fire('epigameChangeSettings')"});
+	var postQuizLabel = $(createElement(document, 'label', {id:'postQuizLabel', "for":"testTimeToggle"})).text('Time Limit for the Entire Test');	
+	
+//	var warpSettingDiv = $(createElement(document, 'div', {id:'warpSettingDiv'}));
+//	var warpSettingLabel = $(createElement(document, 'span', {id:'quizStringLabel'})).text('Quiz Selector:'); 
+	
+	quizStringDiv.append(quizStringLabel).append(createBreak()).append(preQuizToggle).append(preQuizLabel);	
 	
 	//Project settings input
 	var settingsToggle = createElement(document, "input", {id:'settingsToggle', type:"checkbox", checked:"checked", onclick:"eventManager.fire('epigameToggleSettings')"});
@@ -141,8 +133,6 @@ View.prototype.EpigameNode.generatePage = function(view){
 	var settingsSpatialToggle = createElement(document, "input", {id: 'spatialToggle', type:"checkbox", onclick:"eventManager.fire('epigameChangeSettings')"});
 	var settingsSpatialLabel = $(createElement(document, 'label', {id:'spatialLabel', "for":"spatialToggle"})).text('Use Spatial Interface');
 
-	var rankValue = $(createElement(document, 'label', {id:'rankText', "for":"rankText"})).text('Value');
-
 	var rank1Name = $(createElement(document, 'label', {id:'rank1Name', "for":"rank1Name"})).text('Fluffy');
 	var rank2Name = $(createElement(document, 'label', {id:'rank2Name', "for":"rank2Name"})).text('Planet Hopper');
 	var rank3Name = $(createElement(document, 'label', {id:'rank3Name', "for":"rank3Name"})).text('Stratosplorer');
@@ -156,6 +146,12 @@ View.prototype.EpigameNode.generatePage = function(view){
 	var rank5Text = createElement(document, "input", {id:'rank5Text', type:"text", onchange:"eventManager.fire('epigameChangeSettings')",size:"5"});
 
 	var rankValue = $(createElement(document, 'label', {id:'rankText', "for":"rankText"})).text('Rank Values');
+
+	var forceRestrictionToggle = createElement(document, "input", {id:'forceRestrictionToggle', type:"checkbox", onclick:"eventManager.fire('epigameChangeSettings')"});	
+	var forceRestrictionLabel = $(createElement(document, 'label', {id:'forceRestriction', "for":"forceRestriction"})).text('Disable Force Restrictions');
+
+	var hideQuizScoreToggle = createElement(document, "input", {id:'hideQuizScoreToggle', type:"checkbox", onclick:"eventManager.fire('epigameChangeSettings')"});	
+	var hideQuizScoreLabel = $(createElement(document, 'label', {id:'hideQuizScore', "for":"hideQuizScore"})).text('Hide Quiz Score');
 	
 	//get document mode
 	var levelString = "";
@@ -166,9 +162,6 @@ View.prototype.EpigameNode.generatePage = function(view){
 		//get the existing level string
 		levelString = this.content.levelString;
 		
-		/*if(this.content.useCustomSwf != 'undefined'){
-			useCustomSwf = this.content.useCustomSwf;
-		}*/
 		if (this.content.customUri){
 			customUri = this.content.customUri;
 		}
@@ -193,6 +186,9 @@ View.prototype.EpigameNode.generatePage = function(view){
 	settingsDiv.append(createBreak()).append(rankValue).append(createBreak());
 	settingsDiv.append(rank1Text).append(rank1Name).append(createBreak()).append(rank2Text).append(rank2Name).append(createBreak()).append(rank3Text).append(rank3Name).append(createBreak()).append(rank4Text).append(rank4Name).append(createBreak()).append(rank5Text).append(rank5Name).append(createBreak());
 
+	settingsDiv.append(createBreak()).append(forceRestrictionToggle).append(forceRestrictionLabel).append(createBreak());
+	settingsDiv.append(createBreak()).append(hideQuizScoreToggle).append(hideQuizScoreLabel).append(createBreak());
+	
 	//add the authoring components to the page
 	$(pageDiv)
 	.append(swfUrlDiv)
@@ -201,16 +197,9 @@ View.prototype.EpigameNode.generatePage = function(view){
 	.append(modeSelectorDiv)
 	.append(createBreak());
 	
-	/*
-	if(mode == "adaptiveQuiz") {
-		$(pageDiv)
-		.append(questionDiv)
-		.append(createBreak());
-	}
-	*/
-	
 	$(pageDiv)
 	.append(levelStringDiv)
+	.append(quizStringDiv)
 	.append(createBreak())
 	.append(createBreak())
 	.append(settingsToggle)
@@ -219,70 +208,14 @@ View.prototype.EpigameNode.generatePage = function(view){
 	
 	//add the page to the parent
 	parent.appendChild(pageDiv);
-	
-	//$("#dynamicPage").append("<p>Hola</a>");
-	
-	// append script used to communicate with flash
-	/*
-	var jsFunctions = '<script type="text/javascipt">\nfunction receiveLevelData(value) {\n'+
-		'alert("receiveleveldata");\n'+
-		'//sendDataToGame(value);\n' +
-	'};\n</script>\n';
-	*/
-	//$("#dynamicPage").append(jsFunctions);
-	 
-	/*
-	// identify the Flash applet in the DOM - Provided by Adobe on a section on their site about the AS3 ExternalInterface usage.
-	function thisMovie(movieName) {
-		if (navigator.appName.indexOf("Microsoft") != -1) {
-			return window[movieName];
-		} else {
-			return document[movieName];
-		}
-	}
-
-
-	// Call as3 function in identified Flash applet
-	function sendDataToGame(value) {
-		// Put the string at the bottom of the page, so I can see easily what data has been sent
-		document.getElementById("outputdiv").innerHTML = "<b>Level Data Sent to Game:</b> "+value; 
-
-		// Use callback setup at top of this file.
-		thisMovie("epigame").sendToGame(value);
-	}
-	*/
-	
-		
+			
 	//populate the fields
 	$('#swfUrlInput').val(customUri);
 	$('#levelStringTextArea').val(levelString);
 	$('#modeSelector').val(mode);
 	
-	// get the source setting from the content and select corresponding radio button
-	/*
-	$('input[name="sourceSelect"]').each(function(){
-		if($(this).val() == useCustomSwf){
-			$(this).prop('checked',true);
-		}
-	});
-	*/
 	this.updateSettingsDisplay();
 	this.updateModeSelection();
-	
-	//get the url from the content and set it into the authoring textarea
-	/*
-	var authoringSwfHtml = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="770" height="480" id="leveleditor" align="middle">'
-	+ '<param name="allowScriptAccess" value="sameDomain" />'
-	+ '<param name="allowFullScreen" value="false" />'
-	+ '<param name="movie" value="/vlewrapper/vle/node/epigame/leveleditor.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" />	<embed src="/vlewrapper/vle/node/epigame/leveleditor.swf" quality="high" bgcolor="#ffffff" width="770" height="480" name="leveleditor" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />'
-	+ '</object>';
-	
-	$('#authoringSwfDiv').html(authoringSwfHtml);
-	//set change event listener for source radio buttons
-	$('input[name="sourceSelect"]').change(function(){
-		eventManager.fire('epigameUpdateSource');
-	});
-	*/
 };
 
 /**
@@ -292,7 +225,6 @@ View.prototype.EpigameNode.generatePage = function(view){
  */
 View.prototype.EpigameNode.importLevelStringToEditor = function() {
 	var levelString = this.content.levelString;
-	//thisMovie("leveleditor").editorImport(levelString);
 };
 
 /**
@@ -309,10 +241,6 @@ function receiveLevelData(value) {
 
 // Call as3 function in identified Flash applet
 function sendDataToGame(value) {
-	// Put the string at the bottom of the page, so I can see easily what data has been sent
-	//document.getElementById("outputdiv").innerHTML = "<b>Level Data Sent to Game:</b> "+value; 
-	// Use callback setup at top of this file.
-	//thisMovie("epigame").sendToGame(value);
 };
 
 /**
@@ -362,19 +290,6 @@ View.prototype.EpigameNode.updateSwfUrl = function() {
  * Updates the source mode based on user input
  */
 View.prototype.EpigameNode.updateSwfSource = function(){
-	// update content
-	/*
-	var useCustom = $('input[name="sourceSelect"]:checked').val();
-	this.content.useCustomSwf = useCustom;
-	
-	if(useCustom == 'true'){
-		$('#swfUrlDiv').show();
-		$('#authoringSwfDiv').hide();
-	} else {
-		$('#swfUrlDiv').hide();
-		$('#authoringSwfDiv').show();
-	}
-	*/
 	
 	// fire source updated event, this will update the preview
 	this.view.eventManager.fire('sourceUpdated');
@@ -394,6 +309,9 @@ View.prototype.EpigameNode.updateModeSelection = function() {
 	var dataDiv = $('#levelStringDiv');
 	var dataLabel = $('#levelStringLabel');
 	var dataField = $('#levelStringTextArea');
+
+	var	quizDiv = $('#quizStringDiv');
+	var	quizLabel = $('#quizStringLabel');			
 	
 	switch (index) {
 		//Use data value as mission string
@@ -401,21 +319,33 @@ View.prototype.EpigameNode.updateModeSelection = function() {
 		case 4://Editor
 			dataLabel.text("Mission Data String:");
 			dataDiv.show();
+			
+			quizLabel.text("");
+			quizDiv.hide();					
 			break;
-		
-		/*
 		//Use data value as adaptive index/identifier
 		case 2://Adaptive Mission
-		case 3://Adaptive Quiz
-			dataLabel.text("Mission/Question Index (integer):");
+			dataLabel.text("Warp Mission Number:");
 			dataDiv.show();
+			
+			quizLabel.text("");
+			quizDiv.hide();							
 			break;
-		*/
+		case 3://Adaptive Quiz
+			dataLabel.text("");
+			dataDiv.hide();		
+		
+			quizLabel.text("Quiz Selector:");
+			quizDiv.show();
+			break;
 		
 		//Ignore data value
 		default:
 			dataLabel.text("");
 			dataDiv.hide();
+			
+			quizLabel.text("");
+			quizDiv.hide();								
 			break;
 	}
 	
@@ -444,7 +374,9 @@ View.prototype.EpigameNode.updateSettings = function() {
 			rank2Val: $("#rank2Text").val(),
 			rank3Val: $("#rank3Text").val(),
 			rank4Val: $("#rank4Text").val(),
-			rank5Val: $("#rank5Text").val()				
+			rank5Val: $("#rank5Text").val(),
+			forceRestriction: Boolean($("#forceRestrictionToggle").prop("checked")),
+			hideQuizScore: Boolean($("#hideQuizScoreToggle").prop("checked"))
 		};
 	} else {
 		delete this.content.settings;
@@ -473,7 +405,7 @@ View.prototype.EpigameNode.updateSettingsDisplay = function() {
 		$("#scoreReqsToggle").prop("checked", Boolean(this.content.settings.globalizeReqs));
 		$("#questionsToggle").prop("checked", Boolean(this.content.settings.showQuestions));
 		$("#noQuestionsToggle").prop("checked", Boolean(this.content.settings.showNoQuestions));
-		$("#spatialToggle").prop("checked", Boolean(this.content.settings.spatialInterface));		
+		$("#spatialToggle").prop("checked", Boolean(this.content.settings.spatialInterface));	
 		$("#noTimeToggle").prop("checked", Boolean(this.content.settings.noTime));
 		$("#testTimeToggle").prop("checked", Boolean(this.content.settings.testTime));
 		$("#questionTimeToggle").prop("checked", Boolean(this.content.settings.questionTime));
@@ -484,6 +416,8 @@ View.prototype.EpigameNode.updateSettingsDisplay = function() {
 		$("#rank3Text").val(this.content.settings.rank3Val);
 		$("#rank4Text").val(this.content.settings.rank4Val);
 		$("#rank5Text").val(this.content.settings.rank5Val);
+		$("#forceRestrictionToggle").prop("checked", Boolean(this.content.settings.forceRestriction));	
+		$("#hideQuizScoreToggle").prop("checked", Boolean(this.content.settings.hideQuizScore));
 
 		//Settings enabled, so check the checkbox and show UI
 		$("#settingsToggle").prop("checked", true);
@@ -518,7 +452,9 @@ View.prototype.EpigameNode.toggleSettings = function() {
 			rank2Val: 1200,
 			rank3Val: 3600,
 			rank4Val: 7200,
-			rank5Val: 11000
+			rank5Val: 11000,
+			forceRestriction: false,
+			hideQuizScore: false
 		};
 		
 		console.log("applying ui settings");
@@ -540,6 +476,8 @@ View.prototype.EpigameNode.toggleSettings = function() {
 		$("#rank3Text").val(this.content.settings.rank3Val);
 		$("#rank4Text").val(this.content.settings.rank4Val);
 		$("#rank5Text").val(this.content.settings.rank5Val);
+		$("#forceRestrictionToggle").val(this.content.settings.forceRestrictionToggle);
+		$("#hideQuizScoreToggle").val(this.content.settings.hideQuizScoreToggle);
 		
 		//Show UI
 		$("#settingsDiv").show();
